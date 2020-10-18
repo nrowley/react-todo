b1b2c242938ccde7ffac6ebf1e8188d991a088bc
nrowley <nelsonononivu2002@gmail.com>
2020-10-18 21:58:34 +0100

    intial commit

---
+++ src/components/Todos.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+/*
+Component that holds the other todos.
+*/
+
+export default function Todos() {
+  const [todos, setTodos] = useState(null);
+
+  return (
+    <div className="container-fluid">
+      <div className="container text-align-center">
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
+}
